72cddf21a5cd8c98172f0d15f2da8fa25890c3f0
M-UNIT <mugabevedward@gmail.com>
2015-10-18 11:22:59 +0300

    catch and try work

---
+++ src/catchand.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mugabe
+ */
+import java.util.Scanner;
+public class catchand {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+	try{
+		System.out.println("please enter a number.....");
+		int userinput = sc.nextInt();
+		System.out.println(UserInput/10);
+	}catch(Exception e){
+		System.out.println("Error");
+	}
+	}
+}
